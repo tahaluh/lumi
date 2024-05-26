2f518fd5975e85578bfd39760851c7eb349aff84
Thauã Magalhães <thauanlucascpl@gmail.com>
2024-05-26 01:28:59 -0300

    feat: eletricity bill model updated

---
+++ api/src/database/migrations/20240526042456-update-eletricity-bill-columns.js
@@ -0,0 +1,115 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('ElectricityBills', 'month');
+    await queryInterface.removeColumn('ElectricityBills', 'consumedElectricity');
+    await queryInterface.removeColumn('ElectricityBills', 'totalAmount');
+    await queryInterface.removeColumn('ElectricityBills', 'energyCompensated');
+    await queryInterface.removeColumn('ElectricityBills', 'details');
+    await queryInterface.addColumn('ElectricityBills', 'referenceMonth', {
+      type: Sequelize.STRING(50),
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'dueDate', {
+      type: Sequelize.STRING(50),
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'energyAmount', {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'energyPrice', {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'energyTotal', {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'energyICMSAmount', {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'energyICMSPrice', {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'energyICMSTotal', {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'energyCompensatedAmount', {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'energyCompensatedPrice', {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'energyCompensatedTotal', {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'publicLightingContribution', {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'totalPrice', {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'barCode', {
+      type: Sequelize.STRING(50),
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'pdfUrl', {
+      type: Sequelize.STRING(255),
+      allowNull: true,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'pdfText', {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.addColumn('ElectricityBills', 'month', {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'consumedElectricity', {
+      type: Sequelize.FLOAT,
+      allowNull: false,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'totalAmount', {
+      type: Sequelize.FLOAT,
+      allowNull: false,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'energyCompensated', {
+      type: Sequelize.FLOAT,
+      allowNull: false,
+    });
+    await queryInterface.addColumn('ElectricityBills', 'details', {
+      type: Sequelize.STRING(400),
+      allowNull: false,
+    });
+    await queryInterface.removeColumn('ElectricityBills', 'referenceMonth');
+    await queryInterface.removeColumn('ElectricityBills', 'dueDate');
+    await queryInterface.removeColumn('ElectricityBills', 'energyAmount');
+    await queryInterface.removeColumn('ElectricityBills', 'energyPrice');
+    await queryInterface.removeColumn('ElectricityBills', 'energyTotal');
+    await queryInterface.removeColumn('ElectricityBills', 'energyICMSAmount');
+    await queryInterface.removeColumn('ElectricityBills', 'energyICMSPrice');
+    await queryInterface.removeColumn('ElectricityBills', 'energyICMSTotal');
+    await queryInterface.removeColumn('ElectricityBills', 'energyCompensatedAmount');
+    await queryInterface.removeColumn('ElectricityBills', 'energyCompensatedPrice');
+    await queryInterface.removeColumn('ElectricityBills', 'energyCompensatedTotal');
+    await queryInterface.removeColumn('ElectricityBills', 'publicLightingContribution');
+    await queryInterface.removeColumn('ElectricityBills', 'totalPrice');
+    await queryInterface.removeColumn('ElectricityBills', 'barCode');
+    await queryInterface.removeColumn('ElectricityBills', 'pdfUrl');
+    await queryInterface.removeColumn('ElectricityBills', 'pdfText');
+  }
+};
--- api/src/database/models/ElectricityBill.ts
+++ api/src/database/models/ElectricityBill.ts
@@ -3,24 +3,48 @@ import sequelize from '../index';
 
 interface ElectricityBillAttributes {
 	uuid: string;
-	clientNumber: string;
-	month: string;
-	consumedElectricity: number;
-	totalAmount: number;
-	energyCompensated: number;
-	details: string;
+	clientNumber: string | null;
+	intallationNumber: string | null;
+	referenceMonth: string | null;
+	dueDate: string | null;
+	energyAmount: number | null;
+	energyPrice: number | null;
+	energyTotal: number | null;
+	energyICMSAmount: number | null;
+	energyICMSPrice: number | null;
+	energyICMSTotal: number | null;
+	energyCompensatedAmount: number | null;
+	energyCompensatedPrice: number | null;
+	energyCompensatedTotal: number | null;
+	publicLightingContribution: number | null;
+	totalPrice: number | null;
+	barCode: string | null;
+	pdfUrl: string | null;
+	pdfText: string | null;
 }
 
 interface ElectricityBillCreationAttributes extends Optional<ElectricityBillAttributes, 'uuid'> { }
 
 class ElectricityBill extends Model<ElectricityBillAttributes, ElectricityBillCreationAttributes> implements ElectricityBillAttributes {
 	public uuid!: string;
-	public clientNumber!: string;
-	public month!: string;
-	public consumedElectricity!: number;
-	public totalAmount!: number;
-	public energyCompensated!: number;
-	public details!: string;
+	public clientNumber!: string | null;
+	public intallationNumber!: string | null;
+	public referenceMonth!: string | null;
+	public dueDate!: string | null;
+	public energyAmount!: number | null;
+	public energyPrice!: number | null;
+	public energyTotal!: number | null;
+	public energyICMSAmount!: number | null;
+	public energyICMSPrice!: number | null;
+	public energyICMSTotal!: number | null;
+	public energyCompensatedAmount!: number | null;
+	public energyCompensatedPrice!: number | null;
+	public energyCompensatedTotal!: number | null;
+	public publicLightingContribution!: number | null;
+	public totalPrice!: number | null;
+	public barCode!: string | null;
+	public pdfUrl!: string | null;
+	public pdfText!: string | null;
 
 	public readonly createdAt!: Date;
 	public readonly updatedAt!: Date;
@@ -35,27 +59,75 @@ ElectricityBill.init(
 		},
 		clientNumber: {
 			type: DataTypes.STRING(50),
-			allowNull: false,
+			allowNull: true,
 		},
-		month: {
+		intallationNumber: {
 			type: DataTypes.STRING(50),
-			allowNull: false,
+			allowNull: true,
 		},
-		consumedElectricity: {
+		referenceMonth: {
+			type: DataTypes.STRING(50),
+			allowNull: true,
+		},
+		dueDate: {
+			type: DataTypes.STRING(50),
+			allowNull: true,
+		},
+		energyAmount: {
 			type: DataTypes.FLOAT,
-			allowNull: false,
+			allowNull: true,
 		},
-		totalAmount: {
+		energyPrice: {
 			type: DataTypes.FLOAT,
-			allowNull: false,
+			allowNull: true,
 		},
-		energyCompensated: {
+		energyTotal: {
 			type: DataTypes.FLOAT,
-			allowNull: false,
+			allowNull: true,
 		},
-		details: {
-			type: DataTypes.STRING(400),
-			allowNull: false,
+		energyICMSAmount: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+		energyICMSPrice: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+		energyICMSTotal: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+		energyCompensatedAmount: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+		energyCompensatedPrice: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+		energyCompensatedTotal: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+		publicLightingContribution: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+		totalPrice: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+		barCode: {
+			type: DataTypes.STRING(50),
+			allowNull: true,
+		},
+		pdfUrl: {
+			type: DataTypes.STRING(255),
+			allowNull: true,
+		},
+		pdfText: {
+			type: DataTypes.TEXT,
+			allowNull: true,
 		},
 	},
 	{
@@ -67,3 +139,4 @@ ElectricityBill.init(
 );
 
 export { ElectricityBill, ElectricityBillAttributes, ElectricityBillCreationAttributes };
+
